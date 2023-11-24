378a044ae1c0a99b06d747fd0458fe0cef96710a
Adimachukwu Benjamin <thegreatben27@gmail.com>
2023-11-24 16:10:22 +0100

    final   commit fo the car hub App

---
+++ components/index.tsx
@@ -0,0 +1,22 @@
+import Hero from "./Hero";
+import customButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import Cardetails from "./Cardetails";
+import ShowMore from "./ShowMore";
+export{
+    Hero,
+    customButton,
+    Navbar,
+    Footer,
+    SearchBar,
+    CustomFilter,
+    SearchManufacturer,
+    CarCard,
+    Cardetails,
+    ShowMore
+}
